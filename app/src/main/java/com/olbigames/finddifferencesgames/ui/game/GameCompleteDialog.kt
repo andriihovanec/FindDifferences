ba21e4ef3e7277411c4e4b87fbc6da36081af63e
Dmytro <dmytrokutko@gmail.com>
2020-06-03 22:43:04 +0300

    finished offline advertising

--- app/src/main/java/com/olbigames/finddifferencesgames/ui/game/GameCompleteDialog.kt
+++ app/src/main/java/com/olbigames/finddifferencesgames/ui/game/GameCompleteDialog.kt
@@ -2,18 +2,24 @@ package com.olbigames.finddifferencesgames.ui.game
 
 import android.animation.ObjectAnimator
 import android.app.Dialog
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
 import com.google.android.gms.ads.AdRequest
 import com.olbigames.finddifferencesgames.R
 import com.olbigames.finddifferencesgames.extension.invisible
+import com.olbigames.finddifferencesgames.utilities.BannerGenerator
+import com.olbigames.finddifferencesgames.utilities.ConnectionUtil
 import kotlinx.android.synthetic.main.dialog_game_complete.*
 import kotlinx.android.synthetic.main.dialog_game_complete.view.*
 
@@ -39,13 +45,25 @@ class GameCompleteDialog : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.dialog_game_complete, container, false)
-        val adRequest =
-            AdRequest.Builder().build()
-        view.adView2.loadAd(adRequest)
         val anim = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f)
         anim.duration = 1000
         anim.start()
         return view
+    }
+
+    private fun openMarket() {
+        val uri = Uri.parse("market://search?q=pub:Olbi Games")
+        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+        try {
+            startActivity(goToMarket)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/search?q=pub:Olbi Games")
+                )
+            )
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -55,6 +73,20 @@ class GameCompleteDialog : DialogFragment() {
         ib_free_hints.setOnClickListener {
             listener.onDialogFreeHintsGameClick()
             ib_free_hints.invisible()
+        }
+        if (ConnectionUtil.isNetworkAvailable(context!!)) {
+            val adRequest =
+                AdRequest.Builder().build()
+            adView2.loadAd(adRequest)
+            ivBanner.visibility = View.GONE
+        } else {
+            adView2.visibility = View.GONE
+            Glide.with(context!!)
+                .load(BannerGenerator.getBanner(resources))
+                .into(ivBanner)
+            ivBanner.setOnClickListener {
+                openMarket()
+            }
         }
     }
 
